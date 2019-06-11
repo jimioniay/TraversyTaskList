33bd9024cbc89d3e78694f8e4ae5e010cf1422ec
Jimi-Oni Ayomide <jimioniay@gmail.com>
2019-06-11 20:40:50 +0100

    included search imp for tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,26 @@
 
 documentInput = document.querySelector("#task");
 documentForm = document.querySelector("form");
+documentFormTasks = document.querySelector("#form-tasks");
 documentUl = document.querySelector(".list-group");
 documentClearBtn = document.querySelector(".clear-tasks");
 documentFilter = document.querySelector("#filter");
 
 
-console.log("documentForm: " + documentForm.submit);
+
 const fnListenAllEvents = () => {
-    documentForm.addEventListener("submit",addTasks);
+    //Add Tasks
+    documentForm.addEventListener("submit", addTasks);
+    //Delete a task
+    documentFormTasks.addEventListener('click', deleteTask);
+    //Clear all tasks
+    documentClearBtn.addEventListener("click", clearAllTasks);
+    // Filter tasks
+    documentFilter.addEventListener("keyup", filterTasks)
 }
 
 const addTasks = (e) => {
-    if (documentInput.value === ""){
+    if (documentInput.value === "") {
         alert("Please include a task");
     }
     else {
@@ -27,15 +35,48 @@ const addTasks = (e) => {
         li.appendChild(document.createTextNode(documentInput.value)); //append li to inpuut
         li.className = "list-group-item"; // add classname to li
         const link = document.createElement("a"); // create link element
-        link.className= "float-right";
+        link.className = "float-right";
         const i = document.createElement("i"); // create i element for fontawesome
         i.className = "fa fa-trash";// add font awesome class to style the list item
         link.appendChild(i);
-        li.appendChild(link);        
+        li.appendChild(link);
         documentUl.appendChild(li);
         documentInput.value = "";
     }
     e.preventDefault();
+}
+
+const deleteTask = (e) => {
+    if (e.target.className.includes("fa-trash")) {
+        console.log(e.target.className);
+        if (confirm("Are you sure?")) {
+            console.log(e.target.parentElement.parentElement);
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+const clearAllTasks = (e) => {
+    if (confirm("Are you sure you want to clear all tasks? ")) {
+        while (documentUl.firstChild) {
+            documentUl.firstChild.remove();
+        }
+    }
+}
+
+const filterTasks = (e) => {
+    let searchValue = e.target.value.toLowerCase();
+    const listItems = document.querySelectorAll(".list-group-item");
+    listItems.forEach(listItem => {
+        let task = listItem.firstChild.textContent.toLowerCase();
+        console.log("task.includes(searchValue) : "+  task.includes(searchValue) );
+        if (task.includes(searchValue) === true) {
+            listItem.classList.add("d-inline");
+        }
+        else {
+            listItem.classList.add("d-none");
+        }
+    });
 }
 
 const events = fnListenAllEvents();
